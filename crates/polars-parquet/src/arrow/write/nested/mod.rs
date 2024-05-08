96ff5bdda41e0fe96862f4600377d7fd6f2ee34e
thalassemia <cheah_sean@yahoo.com>
2024-05-07 23:22:27 -0700

    fix(rust): Accurate max rep/def level

--- crates/polars-parquet/src/arrow/write/nested/mod.rs
+++ crates/polars-parquet/src/arrow/write/nested/mod.rs
@@ -92,7 +92,7 @@ fn max_def_level(nested: &[Nested]) -> usize {
             Nested::List(nested) => 1 + (nested.is_optional as usize),
             Nested::LargeList(nested) => 1 + (nested.is_optional as usize),
             Nested::Struct(_, is_optional, _) => *is_optional as usize,
-            Nested::FixedSizeList { is_optional, .. } => *is_optional as usize,
+            Nested::FixedSizeList { is_optional, .. } => 1 + (*is_optional as usize),
         })
         .sum()
 }
@@ -101,7 +101,9 @@ fn max_rep_level(nested: &[Nested]) -> usize {
     nested
         .iter()
         .map(|nested| match nested {
-            Nested::FixedSizeList { .. } | Nested::LargeList(_) | Nested::List(_) => 1,
+            Nested::List(nested) => nested.is_optional as usize,
+            Nested::LargeList(nested) => nested.is_optional as usize,
+            Nested::FixedSizeList { is_optional, .. } => *is_optional as usize,
             Nested::Primitive(_, _, _) | Nested::Struct(_, _, _) => 0,
         })
         .sum()
